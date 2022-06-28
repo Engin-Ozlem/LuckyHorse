9ef732be6c61b9434da8d07132a4e50463ab31c6
EnginOzlem <engiin.ozlem@gmail.com>
2022-06-28 17:35:55 +0300

    Initial Commit

---
+++ LuckyHorse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuckyHorse
+//
+//  Created by macbookair on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
